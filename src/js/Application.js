5029fdbfaa4ac35dbc46a7af9b5963d64467565f
Стоян Калпакчиев <stoqn.sisimen@gmail.com>
2021-11-19 12:45:32 -0300

    Added _create()

--- src/js/Application.js
+++ src/js/Application.js
@@ -11,8 +11,9 @@ export default class Application extends EventEmitter {
   constructor(beat) {
     super(beat);
 	this._beat = new Beat()
+	this._create();
 	
-	
+	_create() {
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 	
@@ -23,6 +24,7 @@ export default class Application extends EventEmitter {
 
     document.querySelector(".main").appendChild(message);
 	})
+	}
     this.emit(Application.events.READY);
 	this.emit(Beat.events.BIT);
   }
