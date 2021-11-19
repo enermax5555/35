15eb3727381f390ef08ef3b6d7fc01787e0f5c42
Стоян Калпакчиев <stoqn.sisimen@gmail.com>
2021-11-19 13:45:40 -0300

    Fix

--- src/js/Application.js
+++ src/js/Application.js
@@ -13,22 +13,22 @@ export default class Application extends EventEmitter {
 	this._beat = new Beat();
 	this._create();
      
-	this.emit(Application.events.READY);
+	
   }
   _create() {
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 	
 	this._beat.on(Beat.events.BIT, (bit) => {
+		this.emit(Beat.events.BIT);
     const message = document.createElement("div");
     message.classList.add("message");
     message.innerText = lyrics[bit];
 
     document.querySelector(".main").appendChild(message);
-		this.emit(Application.events.READY);
-		this.emit(Beat.events.BIT);
 	})
-	this.emit(Application.events.READY);
-	this.emit(Beat.events.BIT);
+	
   }
+  this.emit(Application.events.READY);
+  this.emit(Beat.events.BIT);
 }
